60fa7e9a8a1f2eb5e0626c3c46e2fa868ddd54aa
King Celestial <139430697+KingCelestial@users.noreply.github.com>
2023-12-18 21:51:08 +0530

    portfolio update and image search website

---
+++ Front-End Projects/Image Search Website/script.js
@@ -0,0 +1,58 @@
+const accessKey = "OptD-2DaU0Bcrf7nu1VkXAsURdH6JlzOP0z3-7ejAYA";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("searchInput");
+const searchResults = document.querySelector(".searchResults");
+const showMore = document.getElementById("showMoreButton");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResults.innerHTML = ""
+    }
+
+// created the same templet as our html has
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("searchResult");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+    if(page > 1){
+        showMore.style.display = "block";
+    }
+}
+
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () =>{
+    searchImages();
+});
--- SauravSharmaPortfolio.com/index.js
+++ SauravSharmaPortfolio.com/index.js
@@ -1,5 +1,5 @@
-let theme = document.getElementById("themeButton");
-let dark = document.querySelector("body");
-theme.addEventListener('click',()=>{
-    dark.classList.toggle('darkTheme');
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
 })
